7c1a4dc42799ba26e4face8c463191f037f2394e
vassdeniss <vassdeniss@gmail.com>
2023-05-19 21:41:01 +0300

    Experimental sliders

--- src/views/home.js
+++ src/views/home.js
@@ -4,7 +4,7 @@ export function renderHome(ctx) {
   ctx.render(homeTemplate());
 }
 
-const homeTemplate = () => html`<link
+const homeTemplate = () => html` <link
     href="../../static/css/home.css"
     rel="stylesheet"
   />
@@ -69,4 +69,44 @@ const homeTemplate = () => html`<link
       </div>
     </div>
     <hr />
+  </div>
+  <div class="container mb-5">
+    <h2>Our Clients</h2>
+    <section class="customer-logos slider">
+      <div class="slide">
+        <img
+          src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"
+        />
+      </div>
+      <div class="slide">
+        <img
+          src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"
+        />
+      </div>
+      <div class="slide">
+        <img
+          src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"
+        />
+      </div>
+      <div class="slide">
+        <img
+          src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"
+        />
+      </div>
+      <div class="slide">
+        <img
+          src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"
+        />
+      </div>
+      <div class="slide">
+        <img
+          src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"
+        />
+      </div>
+      <div class="slide">
+        <img
+          src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"
+        />
+      </div>
+    </section>
   </div>`;
