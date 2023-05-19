3eee1e706d69ca9fd4b8820f40f20c31663d26fc
vassdeniss <vassdeniss@gmail.com>
2023-05-19 21:01:52 +0300

    Example home

--- src/views/layout.js
+++ src/views/layout.js
@@ -1,90 +1,53 @@
-import { html, nothing } from '../lib/lit-html.js';
-import { classMap } from '../lib/directives/class-map.js';
+import { html } from '../lib/lit-html.js';
 
 export const layoutTemplate = (content) => html`<header>
-    <nav
-      class="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark bg-dark border-bottom box-shadow mb-3"
-    >
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Elesar Print</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target=".navbar-collapse"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          class="navbar-collapse collapse d-sm-inline-flex justify-content-between"
-        >
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                asp-area="Customer"
-                asp-controller="Home"
-                asp-action="Index"
-                >Home</a
-              >
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                asp-area="Customer"
-                asp-controller="Home"
-                asp-action="Privacy"
-                >Privacy</a
-              >
-            </li>
-            @if (this.User.IsInRole(Roles.Admin)) {
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Content Managment
-              </a>
-              <ul class="dropdown-menu">
-                <li class="nav-item">
-                  <a
-                    class="dropdown-item"
-                    asp-area="Admin"
-                    asp-controller="Category"
-                    asp-action="Index"
-                    >Category</a
-                  >
-                </li>
-                <li><hr class="dropdown-divider" /></li>
-                <li class="nav-item">
-                  <a
-                    class="dropdown-item"
-                    asp-area="Admin"
-                    asp-controller="Product"
-                    asp-action="Index"
-                    >Product</a
-                  >
-                </li>
-                <li><hr class="dropdown-divider" /></li>
-              </ul>
-            </li>
-            }
-          </ul>
-        </div>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Elesar Print</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExampleDefault"
+        aria-controls="navbarsExampleDefault"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="http://example.com"
+              id="dropdown01"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              >Dropdown</a
+            >
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
-  <main>
-    <div class="container mb-3 pb-3" style="min-height:100vh">
-      <main role="main" class="pb-3">${content}</main>
-    </div>
-  </main>
-  <footer class="border-top footer text-muted bg-primary">
-    <div class="container">&copy; @DateTime.Now.Year - Elesar Print</div>
+  <main role="main">${content}</main>
+  <footer class="container">
+    <p>&copy; Elesar Print 2018-${new Date().getFullYear()}</p>
   </footer>`;
